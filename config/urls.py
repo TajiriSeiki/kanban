945c325203de6227006b4e47977ac75727b10605
TajiriSeiki <seiki.1226@gmail.com>
2022-03-20 16:55:11 +0900

    use template

--- config/urls.py
+++ config/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 app_name = "kanban"
 urlpatterns = [
     path("kanban/", include("kanban.urls")),
+    path('kanban/', include('django.contrib.auth.urls')),
     path('admin/', admin.site.urls),
 ]
