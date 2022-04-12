fc7148a94b0eb21343542dfcca9327da460847d5
TajiriSeiki <seiki.1226@gmail.com>
2022-04-13 00:27:09 +0900

    made css file and apply form-control

--- kanban/forms.py
+++ kanban/forms.py
@@ -3,21 +3,40 @@ from django.contrib.auth.models import User
 from .models import List, Card
 
 class UserForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
     class Meta:
         model = User
         fields = ("username", "last_name", "first_name", "email",)
 
 class ListForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
     class Meta:
         model = List
         fields = ("title",)
 
 class CardForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
     class Meta:
         model = Card
         fields = ("title", "description", "list")
 
+
 class CardCreateFromHomeForm(forms.ModelForm):
+
+    def __init__(self, *args, **kwargs):
+        super(CardCreateFromHomeForm, self).__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'#クラスを指定 bootstrapでいい感じのファームへ
+
     class Meta:
         model = Card
         fields = ("title", "description",)
